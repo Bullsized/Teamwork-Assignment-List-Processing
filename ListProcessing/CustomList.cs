4d48c32d6eb91a29febe1dc64fc397e7efd1644e
Kiril Georgiev <hammer3@abv.bg>
2017-11-17 01:09:04 +0200

    Added Initial Project, List initialization, toString Method

---
+++ ListProcessing/CustomList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListProcessing
+{
+    class CustomList<T> : List<T>
+    {
+        public CustomList(IEnumerable<T> collection)
+            :base(collection) { }
+
+        public override string ToString()
+        {
+            return String.Join(" ", this);
+        }
+    }
+}
---
+++ ListProcessing/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ListProcessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new CustomList<string>(Console.ReadLine().Split());
+            Console.WriteLine(list);
+        }
+    }
+}
